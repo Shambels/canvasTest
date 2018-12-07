f800e00e50dba48ffa2370c14a513085923dfb2b
Shambels <MrChnip@gmail.com>
2018-12-07 12:08:43 +0100

    cursors

---
+++ bonus.js
@@ -0,0 +1,12 @@
+    // dca = Math.sqrt( (mouse.x-ca.x)*(mouse.x-ca.x) + (mouse.y-ca.y)*(mouse.y-ca.y) );
+    // dcb = Math.sqrt( (mouse.x-cb.x)*(mouse.x-cb.x) + (mouse.y-cb.y)*(mouse.y-cb.y) );
+    // dcc = Math.sqrt( (mouse.x-cc.x)*(mouse.x-cc.x) + (mouse.y-cc.y)*(mouse.y-cc.y) );
+    // dcd = Math.sqrt( (mouse.x-cd.x)*(mouse.x-cd.x) + (mouse.y-cd.y)*(mouse.y-cd.y) );    
+    // // If cursor is near a corner,
+    // if ( dca < 13 || dcd < 13 ) {
+    //   canvas.style.cursor = "nwse-resize"
+    // } else if ( dcb < 13 || dcc < 13 ) {      
+    //   canvas.style.cursor = "nesw-resize"
+    // } else {
+    //   canvas.style.cursor = "default"
+    // }
--- canvas.js
+++ canvas.js
@@ -19,68 +19,119 @@ var x2, y2;
 var dx, dy;
 var mx, my;
 var rect;
+var ca= {
+  x: undefined,
+  y: undefined
+}
+var cb= {
+  x: undefined,
+  y: undefined
+}
+var cc= {
+  x: undefined,
+  y: undefined
+}
+var cd= {
+  x: undefined,
+  y: undefined
+}
 
+var corners = [ca,cb,cc,cd];
+
+function orderPositions (a,b) {
+  return a.x - b.x || a.y - b.y
+} 
 
 window.addEventListener('mousemove', () => {
   mouse.x = event.x;
   mouse.y = event.y;
-  // console.log(c.isPointInPath(mouse.x,mouse.y,rect));
-  
-  // If there's a shape already
-  if (x2 && y2 && x0 && y0){
-    // Check Rectangle orientation
-    // CASE 1
-    if (x2>x0 && y2>y0){
-      // Check if mouse is inside Rectangle on click   
-      if ((mouse.x > x0) && (mouse.x < x2) && (mouse.y > y0) && (mouse.y < y2)) {
-        isInside = true;
-        console.log('isInside = ' + isInside);
-      } else {
-        isInside = false;
-        console.log('isInside = ' + isInside);
+  // If there's a RECTANGLE already
+  if (x2 && y2 && x0 && y0) {
+    // find 4 corners coord (ca = origin => cd = end)
+    ca = {
+      x: x0,
+      y: y0
+      }   
+    cb = {
+      x: x2,
+      y: y0
+      }      
+    cc = {
+      x: x0,
+      y: y2
+      }      
+    cd = {
+      x: x2,
+      y: y2
+    }          
+
+    // Check if cursor is inside RECTANGLE
+    if (c.isPointInPath(mouse.x, mouse.y)) {
+      isInside = true;
+      if (!isDrawing && !isMoving){
+        canvas.style.cursor = "grab"
       }
+    } else {
+      isInside = false;
+      canvas.style.cursor = "crosshair";
     }
-    // CASE 2 
-    if (x2>x0 && y2<y0){      
-      if ((mouse.x > x0) && (mouse.x < x2) && (mouse.y < y0) && (mouse.y > y2)) {
-        isInside = true;
-        console.log('isInside = ' + isInside);
-      } else {
-        isInside = false;
-        console.log('isInside = ' + isInside);
-      }
-    }
-    // CASE 3
-    if (x2<x0 && y2<y0){      
-      if ((mouse.x < x0) && (mouse.x > x2) && (mouse.y < y0) && (mouse.y > y2)) {
-        isInside = true;
-        console.log('isInside = ' + isInside);
-      } else {
-        isInside = false;
-        console.log('isInside = ' + isInside);
-      }
-    }
-    // CASE 4
-    if (x2<x0 && y2>y0){       
-      if ((mouse.x < x0) && (mouse.x > x2) && (mouse.y > y0) && (mouse.y < y2)) {
-        isInside = true;
-        console.log('isInside = ' + isInside);
-      } else {
-        isInside = false;
-        console.log('isInside = ' + isInside);
-      }
+    function uselessIsInsideCheck() {
+      // // Check Rectangle orientation
+      // // CASE 1
+      // if (x2>x0 && y2>y0){
+      //   // Check if mouse is inside Rectangle on click   
+      //   if ((mouse.x > x0) && (mouse.x < x2) && (mouse.y > y0) && (mouse.y < y2)) {
+      //     isInside = true;
+      //     console.log('isInside = ' + isInside);
+      //   } else {
+      //     isInside = false;
+      //     console.log('isInside = ' + isInside);
+      //   }
+      // }
+      // // CASE 2 
+      // if (x2>x0 && y2<y0){      
+      //   if ((mouse.x > x0) && (mouse.x < x2) && (mouse.y < y0) && (mouse.y > y2)) {
+      //     isInside = true;
+      //     console.log('isInside = ' + isInside);
+      //   } else {
+      //     isInside = false;
+      //     console.log('isInside = ' + isInside);
+      //   }
+      // }
+      // // CASE 3
+      // if (x2<x0 && y2<y0){      
+      //   if ((mouse.x < x0) && (mouse.x > x2) && (mouse.y < y0) && (mouse.y > y2)) {
+      //     isInside = true;
+      //     console.log('isInside = ' + isInside);
+      //   } else {
+      //     isInside = false;
+      //     console.log('isInside = ' + isInside);
+      //   }
+      // }
+      // // CASE 4
+      // if (x2<x0 && y2>y0){       
+      //   if ((mouse.x < x0) && (mouse.x > x2) && (mouse.y > y0) && (mouse.y < y2)) {
+      //     isInside = true;
+      //     console.log('isInside = ' + isInside);
+      //   } else {
+      //     isInside = false;
+      //     console.log('isInside = ' + isInside);
+      //   }
+      // }
     }
   }
 });
 
 if (isRectangle) {
-
   canvas.addEventListener('mousedown', () => {
     if (isInside === false) {
+      // set Origin on Cursor location
       x0 = event.x;
       y0 = event.y;
       isDrawing = true;
     } else {
+      // set Origin/ Dimensions as previous Shape's. 
+      canvas.style.cursor = "all-scroll";
       isMoving = true;
       dx0 = x0;
       dy0 = y0;
@@ -89,11 +140,13 @@ if (isRectangle) {
     }
     canvas.addEventListener('mousemove', function abc() {
       if (isMoving === false) {
+        // Define New Rectangle dimensions from origin to Cursor.
         x2 = event.x;
         y2 = event.y;
         dx = x2 - x0;
         dy = y2 - y0;
       } else {
+        // 
         mx = dx0 + (mouse.x - mx0);
         my = dy0 + (mouse.y - my0);
         x2 = mx + dx;
@@ -101,11 +154,17 @@ if (isRectangle) {
         x0 = mx;
         y0 = my;
       }
-      if ( isDrawing || isMoving ) {
+      if (isDrawing || isMoving) {
         c.strokeStyle = "rgba(255,255,255,.9)";
         c.setLineDash([5, 15]);
         c.clearRect(0, 0, canvas.width, canvas.height);
-        rect = c.strokeRect(x0, y0, dx, dy);     
+        c.beginPath();
+        rect = c.rect(x0, y0, dx, dy);
+        strokerect = c.strokeRect(x0,y0,dx,dy);
+        corners = [ca,cb,cc,cd];
+        corners.sort(orderPositions);
+        console.table(corners);        
+        // c.stroke();
       }
       canvas.addEventListener('mouseup', () => {
         isDrawing = false;
